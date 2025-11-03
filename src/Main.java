f8fdf12dcfba1c59e07beba6172816866a0d599d
aylin.ak <aylinak@11icloud.com>
2025-11-03 14:50:24 +0300

    kullanıcı ve şifre kontrolü yapildi

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 import java.util.Scanner;
-
-public class Main{//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public static void main(String[] args){
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-
 
             Scanner sc = new Scanner(System.in,"UTF-8");
 
@@ -24,7 +18,7 @@ public static void main(String[] args){
                 System.out.print("Başarısız");
             }
         }
-    }
+
 
 
 
